58468314b202381ea14b307a3f918784cfec37f4
vashisth5 <153975682+vashisth5@users.noreply.github.com>
2024-03-21 12:45:29 +0530

    new update

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -3,23 +3,28 @@ package training.taylor.timetracker.core;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
+import training.taylor.timetracker.core.dao.TimeEntryRepository;
 
 import java.util.List;
 
 @Component
 public class Tracker {
+
+    private final TimeEntryRepository timeEntryRepository;
+
     @Autowired
-    private List<TimeEntry> entries;
+    public Tracker(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
 
     public void add(TimeEntry entry) {
-        entries.add(entry);
+        timeEntryRepository.save(entry);
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
+        if (entries.contains(entry)) {
             entries.remove(entry);
-
-        entries.remove(entry);
+        }
     }
 
     public int size() {
@@ -27,18 +32,6 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
         return entries.get(index);
     }
 }
